b6080a949d895d31a452f6698ec7283e7df45a19
seungyeop Kim <seungyeop2002@gmail.com>
2021-01-26 16:59:25 +0900

    Add first Card "Jacob's law"

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+import data from "../static/data.json";
+
 import Container from "../layout/Container";
+import Grid from "../layout/Grid";
+
+import Card from "./Card";
 
 const App = () => (
   <Container>
     <h1>UI / UX 의 10가지 심리학 법칙</h1>
+    <Grid>
+      {data.map((value) => (
+        <Card {...value} key={value.title} />
+      ))}
+    </Grid>
   </Container>
 );
 
